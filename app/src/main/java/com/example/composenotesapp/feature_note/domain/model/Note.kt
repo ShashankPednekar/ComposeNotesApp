c1a4000bfe6b9d38c4dbfbbfad38c35d011afe79
ShashankPednekar <pednekarshashank33@gmail.com>
2024-11-16 19:41:00 +0530

    room database setup with Note table and its dao

---
+++ app/src/main/java/com/example/composenotesapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package com.example.composenotesapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.composenotesapp.ui.theme.BabyBlue
+import com.example.composenotesapp.ui.theme.LightGreen
+import com.example.composenotesapp.ui.theme.RedOrange
+import com.example.composenotesapp.ui.theme.RedPink
+import com.example.composenotesapp.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
